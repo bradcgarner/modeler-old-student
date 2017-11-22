ff40b54d912d5f11dcd342eaf181844b5ad05aeb
bradcgarner <brad@bradgarner.com>
2017-11-22 13:05:42 -0500

    1st draft multiple csv

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ with open('inputs.json') as f:
 
 
 class Surface:
-  def __init__(self, duration,rainIntensityIncrement,rainIntensityIncrements,vwcIncrement,surface):
+  def __init__(self, duration,rainIntensityIncrement,rainIntensityIncrements,vwcIncrement,surfaceName):
     self.amc = 0  # antecedent moisture condition g/sf
     self.duration = duration # minutes
     self.rainIntensityIncrement = rainIntensityIncrement # g/sf/min, 0.0007, 0.0014, 0.0021
@@ -18,7 +18,7 @@ class Surface:
     self.controlledHi = 70     # vwc (integer 0-100)
     self.controlledRate = 0.05 # g/sf/min
     
-    self.surfaceName =  surface
+    self.surfaceName =  surfaceName
     self.surface =      data['surfaces'][self.surfaceName] # key of product in array
     self.runoffToName = self.surface['runoff']
     self.cdaName =      self.surface['cda']    # contributing drainage area, i.e. uncontrolled runoff from
@@ -27,8 +27,12 @@ class Surface:
 
     self.product =      data['products'][self.productName]  # key of product in array
     self.capacity =     self.product['capacityGsf']   # g/sf
+    self.type =         self.product['type']
     
-    self.etTable =      data['et'][self.etTableName]  # key of product in array
+    if self.etTableName != None:
+      self.etTable =    data['et'][self.etTableName]  # key of product in array
+    else:
+      self.etTable =    None
 
     self.runoff = 0
 
@@ -36,45 +40,57 @@ class Surface:
     self.minutes = 0
     self.minsDay = 24 * 60
     self.minsMonth = 30.5 * self.minsDay
-    self.minsEtSlot = 120
-
+    self.minsEtColumn = 120
     self.output = []
+
+    self.idHeaderTitle = [
+      'surfaceName',
+      '',
+      'productName',
+    ]
+    self.idHeader = [
+      self.surfaceName,
+      '',
+      self.productName,
+    ]
 
     self.initializeHeaderTitle = [
       'Duration',
+      '','',
+      'capacity',
+      'vwcIncrement',
+      '','',
       'rainIntensityIncrement',
       'rainIntensityIncrements',
-      'vwcIncrement',
-
+      '',
+      'cdaName',
+      'controlledRate',
       'controlledLo',
       'controlledHi',
-      'controlledRate',
-    
-      'surfaceName',
+      '',
       'runoffToName',
-      'cdaName',
+      '','',
       'etTableName',
-      'productName',
-
-      'capacity'
+      'minsEtColumn'
     ]
     self.initializeHeader = [
       self.duration,
+      '','',
+      self.capacity,
+      self.vwcIncrement,
+      '','',
       self.rainIntensityIncrement,
       self.rainIntensityIncrements,
-      self.vwcIncrement,
-
+      '',
+      self.cdaName,
+      self.controlledRate,
       self.controlledLo,
       self.controlledHi,
-      self.controlledRate,
-    
-      self.surfaceName,
+      '',    
       self.runoffToName,
-      self.cdaName,
+      '','',
       self.etTableName,
-      self.productName,
-
-      self.capacity
+      self.minsEtColumn
     ]
     self.outputHeader = [
       'month',
@@ -99,7 +115,6 @@ class Surface:
       'retPre',
         
       'hourColumn',
-      'monthRow',
       'etRate',
       'etMax',
       'et',
@@ -107,9 +122,8 @@ class Surface:
       'ret'
     ]
 
-  def cycle(self, rainIntensity, uncontrolled):
-    # rain, uncontrolled are volume in gals/sf/minute
-
+  def cycle(self, rainIntensity, uncontrolled): # rain, uncontrolled are volume in gals/sf/minute
+    
     print(' ')
     print('INITIALIZE')
     print('duration = 5: ',self.duration)
@@ -145,7 +159,7 @@ class Surface:
     print('rain', self.rain)
     print('rainIntensity', rainIntensity)
     
-    intensityColumn = int(rainIntensity/self.rainIntensityIncrement)
+    intensityColumn = int(rainIntensity/self.rainIntensityIncrement) + 1 # start at 1, not 0
     if intensityColumn > self.rainIntensityIncrements - 1:
       intensityColumn = self.rainIntensityIncrements - 1
     print('intensityColumn', intensityColumn)
@@ -188,9 +202,8 @@ class Surface:
     print('preliminary retention' , retPre)
     print('runoff ', self.runoff, ' to ', self.runoffToName)
     
-    hourColumn = int( (self.minutes %  self.minsDay) / self.minsEtSlot ) # calc which 2-hour column we are in
-    monthRow = 6 # get from effective date
-    etRate = self.etTable['table'][monthRow][hourColumn] # gal/sf/min
+    hourColumn = int( (self.minutes %  self.minsDay) / self.minsEtColumn ) # calc which 2-hour column we are in
+    etRate = self.etTable['table'][self.month][hourColumn] # gal/sf/min
     etMax = etRate * duration # gal/sf/min
     # no ET during rain
     if self.rain > 0:
@@ -203,7 +216,6 @@ class Surface:
     print('LOSSES')
     print('effective date') # calculate this
     print('hourColumn', hourColumn)
-    print('monthRow', monthRow)
     print('etRate', etRate)
     print('etMax', etMax)
     print('ET', self.et)
@@ -236,7 +248,6 @@ class Surface:
       retPre,
         
       hourColumn,
-      monthRow,
       etRate,
       etMax,
       self.et,
@@ -252,6 +263,39 @@ class Surface:
     print('amc for next cycle', self.amc)
     print('minutes for next cycle ', self.minutes)
     print('month for next cycle ', self.month)
+
+  def receive(self, uncontrolled): # uncontrolled volume in gals/sf/minute
+    self.minutes += duration
+    self.month = int(self.minutes/self.minsMonth)+1
+    self.input = uncontrolled
+
+    # calculate capacity at start of cycle
+    capacitySpare = self.capacity - self.amc
+    vwc = int((self.amc / self.capacity) * 100 )
+    
+    self.input = uncontrolled
+    
+    self.absorb = self.input
+
+    self.runoff = self.input - self.absorb
+    self.ret = self.amc + self.absorb
+    
+    self.output = [
+      self.month,
+      self.minutes,
+      self.amc,
+      '','','',
+      capacitySpare,
+      '','','',
+      uncontrolled,
+      '',
+      self.input,
+      '',
+      self.absorb,
+      self.runoff,
+      '','','','','',
+      self.ret
+    ]
 
 # create a loop
 # accumulate total rain
@@ -270,27 +314,43 @@ print('rainIntensityIncrement', rainIntensityIncrement)
 print('vwcIncrement', vwcIncrement)
 print('surface', surface)
 
-surface1 = Surface(duration,rainIntensityIncrement,rainIntensityIncrements,vwcIncrement,surface)
+# create and populate dictionary
+surfaces = {}
 
+for surface in data['surfaces']:
+  surfaces[surface] = Surface(duration,rainIntensityIncrement,rainIntensityIncrements,vwcIncrement,surface)
+  
 rainTable = data['events']['rainIntensity']
 
-# with open('model.csv', 'w', newline='') as csvfile:
-#     modelwriter = csv.writer(csvfile, delimiter=' ',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     modelwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-#     modelwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+print('XXXXXXXX')
+print(surfaces)
+print('XXXXXXXX')
 
-# for event in rainTable:
-#   surface1.cycle(event,0)
+# surface1 = Surface(duration,rainIntensityIncrement,rainIntensityIncrements,vwcIncrement,surface)
 
-
-with open('model.csv', 'w', newline='') as csvfile:
+# add header to one file for each surface
+for surface in surfaces:
+  filename = surface + 'Model.csv'
+  with open(filename, 'w', newline='') as csvfile:
     modelwriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    modelwriter.writerow(surface1.initializeHeaderTitle)
-    modelwriter.writerow(surface1.initializeHeader)
+    modelwriter.writerow(surfaces[surface].idHeaderTitle)
+    modelwriter.writerow(surfaces[surface].idHeader)
     modelwriter.writerow([])
-    modelwriter.writerow(surface1.outputHeader)
-    for event in rainTable:
-      surface1.cycle(event,0)
-      modelwriter.writerow(surface1.output)
+    modelwriter.writerow(surfaces[surface].initializeHeaderTitle)
+    modelwriter.writerow(surfaces[surface].initializeHeader)
+    modelwriter.writerow([])
+    modelwriter.writerow(surfaces[surface].outputHeader)
+
+# cycle through each surface for each event in rainTable; order doesn't matter, as long as all cycles 1 occur before all cycles 2, etc.
+for event in rainTable:
+  for surface in surfaces:
+    filename = surface + 'Model.csv'
+    if surfaces[surface].type != 'receiving':
+      surfaces[surface].cycle(event,0)
+    else:
+      surfaces[surface].receive(0)
+    with open(filename, 'w', newline='') as csvfile:
+      modelwriter = csv.writer(csvfile, delimiter=',',
+                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
+      modelwriter.writerow(surfaces[surface].output)
